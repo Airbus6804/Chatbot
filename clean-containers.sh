809555b8e104e48a19b910b09f3118410a420d6e
Ragonesi Alessio <aleragonesi04@gmail.com>
2025-03-30 18:15:14 +0200

    Updated stop contaienrs

--- clean-containers.sh
+++ clean-containers.sh
@@ -1,3 +1,11 @@
+sudo docker stop webapp-chatbot;
+sudo docker stop api;
+sudo docker stop redis;
+sudo docker stop postgres;
+sudo docker stop ollama;
+sudo docker stop nginx;
+sudo docker stop certbot;
+
 sudo docker rm webapp-chatbot;
 sudo docker rm api;
 sudo docker rm redis;
